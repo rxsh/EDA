a9b993241e7c08e52c782bf419e2870290426ab3
rxsh <70354128+rxsh@users.noreply.github.com>
2024-08-28 12:10:13 -0500

    Add files via upload

---
+++ Lab1/histogram.py
@@ -0,0 +1,25 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+try:
+    with open('C:/Users/cesar/Desktop/DS-EDA/Lab1/distancias.txt', 'r') as file:
+        distancias = [float(line.strip()) for line in file]
+except FileNotFoundError:
+    print("El archivo 'distancias.txt' no se encuentra en el directorio.")
+    exit()
+
+distancias_array = np.array(distancias)
+
+min_distancia = 0.0
+max_distancia = np.max(distancias_array)
+
+plt.figure(figsize=(10, 6))
+plt.hist(distancias_array, bins=20, alpha=0.75, color='skyblue', rwidth=0.8, edgecolor='black')
+plt.title('Histograma de la distancias con dimension 5000')
+plt.xlabel('Distancia')
+plt.ylabel('Frecuencia')
+plt.xlim(0,30)
+plt.ylim(0,700)
+plt.grid(True)
+plt.show()
